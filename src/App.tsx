25386f78683e37c4c93858cc4dc7a3f9421e0afe
Akindin <88840107+Akindin@users.noreply.github.com>
2024-04-27 17:31:45 +0300

    Add function to read Barcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import {
+    type MutableRefObject
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import {
     readBarcodesFromImageFile,
     readBarcodesFromImageData,
+    type ReadResult
 } from "zxing-wasm/reader";
+
+function reduceToString(accumulator: string, readResult: ReadResult): string {
+    return accumulator + readResult.text;
+}
+
+async function readImageFromInput(inputRef: MutableRefObject<HTMLInputElement>): Promise<string> {
+    if (inputRef.current.files?.[0]) {
+        const recognition = await readBarcodesFromImageFile(inputRef.current.files[0]);
+        console.log(recognition);
+        return recognition.reduce(reduceToString, "");
+    } else {
+        return "Файл не привязан";
+    }
+}
 
 function App() {
     return (
