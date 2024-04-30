2b72723c28c38ce8236d5823bd999e3eaf70a0eb
Akindin <88840107+Akindin@users.noreply.github.com>
2024-04-30 22:33:02 +0300

    Work in progress

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,10 +1,15 @@
+import parseUrl from "parse-url";
+import { MouseEvent } from "react";
 type URISchemaType = "link" | "text" | "sms" | "phone" | "wifi" | "javascript"
 
-export default function Output({ content }: { content: string }) {
-    function copyToClipboard() {
-        navigator.clipboard.writeText(content);
-    }
 
+
+export default function Output({ content }: { content: string }) {
+    function copyToClipboard(event: MouseEvent) {
+        const button = event.target as HTMLButtonElement;
+        const copiedText = document.querySelector(`[data-id=${button.dataset.for}`)?.textContent;
+        navigator.clipboard.writeText(copiedText ?? "undefined");
+    }
 
 
 
@@ -14,6 +19,12 @@ export default function Output({ content }: { content: string }) {
         const regex = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
 
         console.log(content.match(regex))
+
+        try {
+            console.log(parseUrl(content));
+        } catch (error) {
+            console.log(error)
+        }
 
         if (content.match(regex)) {
             return "link";
@@ -34,33 +45,57 @@ export default function Output({ content }: { content: string }) {
                 </tr>
             )
         } else {
-            return (
-                <tr>
-                    <td>Результат</td>
-                    <td><a href={content}>{content}</a></td>
-                    <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
-                </tr>
-            )
+
+            function mapEntries([key, value]: [string, any]) {
+                return (
+                    <tr>
+                        <td>{key}</td>
+                        <td data-id={key}>{value.toString()}</td>
+                        <td><button onClick={copyToClipboard} data-for={key}>Скопировать в буфер обмена</button></td>
+                    </tr>
+                )
+            }
+
+            console.log(Object.entries(parseUrl(content)).map(mapEntries))
+
+            return [...Object.entries(parseUrl(content)).map(mapEntries)];
+
+            // console.log(
+            //     [<tr>
+            //         <td>Результат</td>
+            //         <td><a href={content}>{content}</a></td>
+            //         <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
+            //     </tr>,
+            //     <tr>
+            //         <td>Результат</td>
+            //         <td><a href={content}>{content}</a></td>
+            //         <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
+            //     </tr>]
+            // )
+
+            // return (
+            //     [<tr>
+            //         <td>Результат</td>
+            //         <td><a href={content}>{content}</a></td>
+            //         <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
+            //     </tr>,
+            //     <tr>
+            //         <td>Результат</td>
+            //         <td><a href={content}>{content}</a></td>
+            //         <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
+            //     </tr>]
+            // )
         }
     }
 
 
-
-    if (type === "text") {
-        return (
-            <output>
-                <table>
-                    <tbody>
-                    </tbody>
-                </table>
-            </output>
-        )
-    } else {
-        return (
-            <output>
-                <a href={content}>{content}</a>
-                <button onClick={copyToClipboard}>Скопировать в буфер обмена</button>
-            </output>
-        )
-    }
+    return (
+        <output>
+            <table>
+                <tbody>
+                    {getContentData()}
+                </tbody>
+            </table>
+        </output>
+    )
 }
