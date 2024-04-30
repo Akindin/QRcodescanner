ec1f37b294708cf5a3a51193e1aabb0a23b8fa5d
Akindin <88840107+Akindin@users.noreply.github.com>
2024-04-30 17:51:33 +0300

    Add copy to clipboard button

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,14 +1,20 @@
 export default function Output({ type, content }: { type: "link" | "text", content: string }) {
+    function copyToClipboard() {
+        navigator.clipboard.writeText(content);
+    }
+
     if (type === "link") {
         return (
             <output>
                 <a href={content}>{content}</a>
+                <button onClick={copyToClipboard}>Скопировать в буфер обмена</button>
             </output>
         )
     } else {
         return (
             <output>
                 <>{content}</>
+                <button onClick={copyToClipboard}>Скопировать в буфер обмена</button>
             </output>
         )
     }
