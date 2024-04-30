2368098c93ecf35fe686e7897866dd0a1edd774e
Akindin <88840107+Akindin@users.noreply.github.com>
2024-04-30 21:06:45 +0300

    Move output display logic to Output component

--- src/App.tsx
+++ src/App.tsx
@@ -17,25 +17,13 @@ function App() {
         setOutput(await readQRCode(file));
     }
 
-    function getType(): "link" | "text" {
-        // [-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
-        // https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
-        const regex = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
-
-        if (output.match(regex)) {
-            return "link";
-        } else {
-            return "text";
-        }
-    }
-
 
 
     return (
         <div className="App">
             <FileInput onFileAccepted={handleFileAccept} onFileRejected={console.log} />
             <Stream onOutputReceived={setOutput} />
-            <Output type={getType()} content={output} />
+            <Output content={output} />
         </div>
     );
 }
--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,19 +1,64 @@
-export default function Output({ type, content }: { type: "link" | "text", content: string }) {
+type URISchemaType = "link" | "text" | "sms" | "phone" | "wifi" | "javascript"
+
+export default function Output({ content }: { content: string }) {
     function copyToClipboard() {
         navigator.clipboard.writeText(content);
     }
 
-    if (type === "link") {
+
+
+
+    function getType(content: string): URISchemaType {
+        // [-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
+        // https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
+        const regex = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
+
+        console.log(content.match(regex))
+
+        if (content.match(regex)) {
+            return "link";
+        } else {
+            return "text";
+        }
+    }
+
+    let type: URISchemaType = getType(content);
+
+    function getContentData() {
+        if (type === "text") {
+            return (
+                <tr>
+                    <td>Результат</td>
+                    <td>{content}</td>
+                    <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
+                </tr>
+            )
+        } else {
+            return (
+                <tr>
+                    <td>Результат</td>
+                    <td><a href={content}>{content}</a></td>
+                    <td><button onClick={copyToClipboard}>Скопировать в буфер обмена</button></td>
+                </tr>
+            )
+        }
+    }
+
+
+
+    if (type === "text") {
         return (
             <output>
-                <a href={content}>{content}</a>
-                <button onClick={copyToClipboard}>Скопировать в буфер обмена</button>
+                <table>
+                    <tbody>
+                    </tbody>
+                </table>
             </output>
         )
     } else {
         return (
             <output>
-                <>{content}</>
+                <a href={content}>{content}</a>
                 <button onClick={copyToClipboard}>Скопировать в буфер обмена</button>
             </output>
         )
