7d65cf9c520685842ba9b910449198d382daccfd
Akindin <88840107+Akindin@users.noreply.github.com>
2024-05-01 16:27:41 +0300

    Fixed styling and Output component

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,14 +1,22 @@
 export default function Output({ type, content }: { type: "link" | "text", content: string }) {
+    if (content === "") {
+        content = "This field will contain information retrieved from the scanned QR code."
+    }
+
     if (type === "link") {
         return (
-            <output>
-                <a href={content}>{content}</a>
+            <output className="output_box">
+                <p>
+                    <a href={content}>{content}</a>
+                </p>
             </output>
         )
     } else {
         return (
-            <output>
-                <>{content}</>
+            <output className="output_box">
+                <p>
+                    {content}
+                </p>
             </output>
         )
     }
