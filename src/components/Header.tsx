6c26a5c77d2efe32e0df02ac93189b6a8ac2577e
Akindin <88840107+Akindin@users.noreply.github.com>
2024-05-01 15:50:53 +0300

    Fixed formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,10 @@ import logo from '../img/logo512.png';
 export default function Header() {
     return (
         <header>
-        <img src={logo} alt="QR Code Scanner" />
-  
-        <div className="logo">
-          <p className="logo-text">QR Code Scanner</p>
-        </div>
-      </header>    
+            <img src={logo} alt="QR Code Scanner" />
+            <div className="logo">
+                <p className="logo-text">QR Code Scanner</p>
+            </div>
+        </header>
     )
 }
