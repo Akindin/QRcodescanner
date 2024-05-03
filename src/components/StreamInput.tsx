98ae22f904c87254b0705b2e388c6ffccbf5c5de
Akindin <88840107+Akindin@users.noreply.github.com>
2024-05-03 07:48:55 +0300

    Add preferation of environment camera

--- src/components/StreamInput.tsx
+++ src/components/StreamInput.tsx
@@ -2,7 +2,7 @@ export default function StreamInput({ onStreamReceived, onStreamRejected }: { on
 
     async function getStream(): Promise<MediaStream | string> {
         try {
-            return await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            return await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false })
         } catch (e) {
             const error = e as Error;
             return error.message;
