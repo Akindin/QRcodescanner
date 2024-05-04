e798a3cdee034a4a66a3bf98df03e2bafb14c95c
Akindin <88840107+Akindin@users.noreply.github.com>
2024-05-04 20:57:58 +0300

    Add clearTimeout in case of succesful videoRef receiving

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -35,6 +35,7 @@ export default function Main() {
             const interval = setInterval(() => {
                 if (videoRef !== null) {
                     clearInterval(interval);
+                    clearTimeout(timeout);
                     resolve()
                 }
             }, 100);
